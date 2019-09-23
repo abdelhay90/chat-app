b2c197c9b0305b588c06fae8dcf3e8a193cafd84
Ahmed Abdelhay <ahmed.abdelhay.90@gmail.com>
2019-09-23 11:07:55 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   BASE_URL: 'http://localhost:3000',
-  googleMapApiKey: 'AIzaSyCrcF39UyNnCA-Rj0fGoo57AjN-RB70ibM',
+  googleMapApiKey: '',
   googleMapsApiEndPoint: 'https://maps.googleapis.com/maps/api/geocode/json'
 };
 
